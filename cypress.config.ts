3056dd2a969264d2620c07f7d65c8c6433a7156f
Serhii <opalinskyiserhii@gmail.com>
2024-03-11 14:25:25 +0200

    add new_tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://example.cypress.io',
+    baseUrl: 'http://localhost:4200',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx}',
     retries: { "runMode": 1, "openMode": 1 },
     defaultCommandTimeout: 6000,
